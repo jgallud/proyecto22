19a8c08b5bfcc4ac70e11b4fa003a817abc12ecd
José A. Gallud <jgallud@gmail.com>
2022-10-03 12:35:37 +0200

    crear unirse partida

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const fs=require("fs");
 const express = require('express');
 const app = express();
+const modelo = require("./servidor/modelo.js");
 
-//HTTP GET POST PUT DELETE
-/*
-get "/"
-get "/obtenerPartidas"
-post get "/agregarUsuario/:nick"
-put "/actualizarPartida"
-delete "/eliminarPartida"
-...
-*/
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send("Hola")
-    .end();
+let juego = new modelo.Juego();
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .send("Hola")
+//     .end();
+// });
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(request,response){
+  var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
+  response.setHeader("Content-type","text/html");
+  response.send(contenido);
+});
+
+app.get("/agregarUsuario/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res=juego.agregarUsuario(nick);
+  response.send(res);
+});
+
+app.get("/crearPartida/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res = juego.jugadorCreaPartida(nick);
+  response.send(res);
+});
+
+app.get("/unirseAPartida/:nick/:codigo",function(request,response){
+  let nick = request.params.nick;
+  let codigo = request.params.codigo;
+  let res = juego.jugadorSeUneAPartida(nick,codigo);
+  response.send(res);
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App está escuchando en el puerto ${PORT}`);
---
+++ servidor/modelo.js
@@ -0,0 +1,96 @@
+function Juego(){
+	this.partidas={};
+	this.usuarios={}; //array asociativo
+
+	this.agregarUsuario=function(nick){
+		let res={nick:-1};
+		if (!this.usuarios[nick]){
+			this.usuarios[nick]=new Usuario(nick,this);
+			res={nick:nick};
+			console.log("Nuevo usuario: "+nick);
+		}
+		return res;
+	}
+	this.eliminarUsuario=function(nick){
+		delete this.usuarios[nick];
+	}
+	this.jugadorCreaPartida=function(nick){
+		let usr = this.usuarios[nick];
+		let res={codigo:-1};
+  		if (usr){
+    		let codigo=usr.crearPartida();
+	    	//let codigo=this.crearPartida(usr);
+	    	res={codigo:codigo};
+	    }
+    	return res;
+	}
+	this.jugadorSeUneAPartida=function(nick,codigo){
+		let usr = this.usuarios[nick];
+		let res={"codigo":-1};
+  		if (usr){
+    		let valor=usr.unirseAPartida(codigo);
+    		//let valor=this.unirseAPartida(codigo,usr)
+	    	res={"codigo":valor};
+	    }
+    	return res;
+	}
+	this.crearPartida=function(usr){
+		let codigo=Date.now();
+		console.log("Usuario "+usr.nick+ " crea partida "+codigo);
+		this.partidas[codigo]=new Partida(codigo,usr); 
+		return codigo;
+	}
+	this.unirseAPartida=function(codigo,usr){
+		let res=-1;
+		if (this.partidas[codigo]){
+			res=this.partidas[codigo].agregarJugador(usr);
+		}
+		else{
+			console.log("La partida no existe");
+		}
+		return res;
+	}
+	this.obtenerPartidas=function(){
+		let lista=[];
+		for (let key in this.partidas){
+			lista.push({"codigo":key,"owner":this.partidas[key].owner});
+		}
+		return lista;
+	}
+	this.obtenerPartidasDisponibles=function(){
+		//devolver sólo las partidas sin completar
+	}
+}
+
+function Usuario(nick,juego){
+	this.nick=nick;
+	this.juego=juego;
+	this.crearPartida=function(){
+		return this.juego.crearPartida(this)
+	}
+	this.unirseAPartida=function(codigo){
+		return this.juego.unirseAPartida(codigo,this);
+	}
+}
+
+function Partida(codigo,usr){
+	this.codigo=codigo;
+	this.owner=usr;
+	this.jugadores=[];
+	this.fase="inicial"; //new Inicial()
+	this.agregarJugador=function(usr){
+		let res=this.codigo;
+		if (this.jugadores.length<2){
+			this.jugadores.push(usr);
+			console.log("El usuario "+usr.nick+" se une a la partida "+this.codigo)
+		}
+		else{
+			res=-1;
+			console.log("La partida está completa")
+		}
+		return res;
+	}
+	this.agregarJugador(this.owner);
+}
+
+module.exports.Juego = Juego;
